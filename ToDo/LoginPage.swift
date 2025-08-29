adfed738c28d3452256983314544788e25485c99
Aadhil Abdulla <aadhil242@gmail.com>
2025-08-29 12:32:58 +0530

    Added Navigation

---
+++ ToDo/LoginPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPage.swift
+//  ToDo
+//
+//  Created by Aadhil Abdulla on 29/08/25.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginPage()
+}
--- ToDo/SignupPage.swift
+++ ToDo/SignupPage.swift
@@ -10,81 +10,92 @@ import SwiftUI
 struct SignupPage: View {
     
     @StateObject var signupViewModel = SignupViewModel()
-
+    @State private var navigateToLogin : Bool = false
     var body: some View {
-        VStack {
-            Text("ToDo CheckMate")
-                .frame(maxWidth : .infinity, alignment: .leading)
-                .font(.system(size: 26))
-                .fontWeight(.heavy)
-                .padding(.vertical)
-            Text("Create a new account")
-                .fontWeight(.medium)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.vertical)
-            
-            HStack{
-                Image(systemName: "envelope")
-                    .frame(width: 20)
-                    .padding(.leading)
-                TextField("Email", text: $signupViewModel.email)
-                    .autocapitalization(.none)
-                    .autocorrectionDisabled()
-                    .keyboardType(.emailAddress)
-                    .padding()
-                    
-            }
-            .background(
-                RoundedRectangle(cornerRadius: 8)
-                    .stroke(.black)
-            )
-            
-           
-            HStack{
-                Image(systemName: "key")
-                    .frame(width: 20)
-                    .padding(.leading)
-                SecureField("Password", text: $signupViewModel.password)
-                    .padding()
-            }
-            .background(
-                RoundedRectangle(cornerRadius: 8)
-                    .stroke(.black)
-            )
-            .padding(.top)
-            
-            if(signupViewModel.responseMessage != ""){
-                Text(signupViewModel.responseMessage)
-                    .foregroundStyle(Color.red)
-                    .padding(.top)
-            }
-            
-            if (signupViewModel.isLoading) {
-                ProgressView()
-                    .padding()
-            } else {
-                Button("Create account"){
-                    Task {
-                        await signupViewModel.signUp()
-                    }
+        
+        NavigationStack{
+            VStack {
+                Text("ToDo CheckMate")
+                    .frame(maxWidth : .infinity, alignment: .leading)
+                    .font(.system(size: 26))
+                    .fontWeight(.heavy)
+                    .padding(.vertical)
+                Text("Create a new account")
+                    .fontWeight(.medium)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.vertical)
+                
+                HStack{
+                    Image(systemName: "envelope")
+                        .frame(width: 20)
+                        .padding(.leading)
+                    TextField("Email", text: $signupViewModel.email)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                        .keyboardType(.emailAddress)
+                        .padding()
+                        
                 }
-                .foregroundStyle(Color.black)
-                .padding()
                 .background(
                     RoundedRectangle(cornerRadius: 8)
                         .stroke(.black)
                 )
-                .padding()
-            }
-            
-            Button("Login In"){
-                //code
-            }
-            .foregroundColor(Color.black)
                 
-            Spacer()
+               
+                HStack{
+                    Image(systemName: "key")
+                        .frame(width: 20)
+                        .padding(.leading)
+                    SecureField("Password", text: $signupViewModel.password)
+                        .padding()
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(.black)
+                )
+                .padding(.top)
+                
+                if(signupViewModel.responseMessage != ""){
+                    Text(signupViewModel.responseMessage)
+                        .foregroundStyle(Color.red)
+                        .padding(.top)
+                        .onAppear{
+                            let generator = UINotificationFeedbackGenerator()
+                            generator.notificationOccurred(.error)
+                        }
+                }
+                
+                if (signupViewModel.isLoading) {
+                    ProgressView()
+                        .padding()
+                } else {
+                    Button("Create account"){
+                        Task {
+                            await signupViewModel.signUp()
+                        }
+                    }
+                    .foregroundStyle(Color.black)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(.black)
+                    )
+                    .padding()
+                }
+                
+                Button("Login In"){
+                    navigateToLogin = true
+                }
+                .foregroundColor(Color.black)
+                Spacer()
+            }
+            .navigationDestination(isPresented: $navigateToLogin ){
+                LoginPage()
+            }
+            .padding()
         }
-        .padding()
+        
+        
     }
 }
 
